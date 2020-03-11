54dad5982b22111b10fedc7b087cffea8e098358
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-03-11 20:06:36 -0300

    Updates

---
+++ precache-manifest.ca9305d0c5295a7abc1d9636c4af96aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a04fbde0c33fe21d22114cbfb689eec",
+    "url": "/URI.CS16.ColorConverter/index.html"
+  },
+  {
+    "revision": "8a7e89682c429ba27ec7",
+    "url": "/URI.CS16.ColorConverter/static/js/2.afbdcd9a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/URI.CS16.ColorConverter/static/js/2.afbdcd9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6102228c902e508df732",
+    "url": "/URI.CS16.ColorConverter/static/js/main.5ef2dec6.chunk.js"
+  },
+  {
+    "revision": "19887c69e790b13d816d",
+    "url": "/URI.CS16.ColorConverter/static/js/runtime-main.51c6ffed.js"
+  }
+]);
